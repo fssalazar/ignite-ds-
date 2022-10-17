24653ad56a31c6ea4a2af4ad51024665694a70c2
fssalazar <felipe.salazar-ext@ab-inbev.com>
2022-10-17 14:42:55 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Checkbox } from './components/Checkbox'
+import './styles/global.css'
+
+export function App() {
+  return (
+    <>
+      <Checkbox />
+      <h1 className='font-bold text-2xl text-cyan-500'>hello world</h1>
+    </>
+  )
+}
+
